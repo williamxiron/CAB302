5bc104ef3a048eb899e57a4ea39dbebdc773328f
KirkReeve <103344016+KirkReeve@users.noreply.github.com>
2022-04-10 13:24:31 +1000

    Add files via upload
    Base Java script, with imports used in the pracs so far.

---
+++ Major Project/src/CAB302/Base/Main.java
@@ -0,0 +1,21 @@
+package CAB302.Base;
+
+//for testing
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+//for GUI
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//for keyboard input
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
